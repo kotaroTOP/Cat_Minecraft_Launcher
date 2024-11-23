963fa728e01a3085b6111002f6c4bbfa25f9e741
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2024-11-23 20:39:54 +0300

    Update CatMCL.py

--- CatMCL.py
+++ CatMCL.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import QThread, pyqtSignal
 class DownloadThread(QThread): 
     progress_changed = pyqtSignal(int) 
     forge_ver = ""
-    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_", "-", "к", "о", "т", "я", "р", "а", "п", "/"]
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_", "-", "к", "о", "т", "я", "р", "а", "п", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\", "|", "(", ")", ".", "[", "]"]
     def get_username(self, filename):
         newFilename = ""
         name = ""
